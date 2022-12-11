6365cb6d952c8af9ab1eaa577de883ae7508de87
pranavsinghrio <112401168+pranavsinghrio@users.noreply.github.com>
2022-12-11 18:50:51 +0530

    message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
-  
+  public static class A{
+      public String met(){
+          return "Invoking method from class A";
+      }
+  }
+  public static class B extends A{
+      public String met(){
+          return "Method is overridden in Extendend class B";
+      }
+  }
+
+    public static void main(String[] args) {
+        B b = new B();
+          b.met();
+    }
+
 }
